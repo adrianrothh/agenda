743497caeda455467393c75f30ee2c33ea460bba
adrianrothh <167586506+adrianrothh@users.noreply.github.com>
2024-06-19 22:55:28 -0300

    Update agenda.c

--- agenda.c
+++ agenda.c
@@ -4,8 +4,8 @@
 
 
 typedef struct agenda{ //Typedef serve para definir um nome para simplificar, e o nome é definido no final, nesse está contato
-    char Nome[30]; 
-    int Numero[11]; //11 = 47 99999 9999 sem os espaços
+    char nome[30]; 
+    int numero[11]; //11 = 47 99999 9999 sem os espaços
 }contato;
 
 
@@ -14,39 +14,65 @@ typedef struct agenda{ //Typedef serve para definir um nome para simplificar, e 
 void addContato(void);
 
 
-
 int main(void){
     //variaveis
     char opcao;
 
     do {
         printf("---------------------------------------\n");
-		printf("       AGENDA DE CONTATOS - MENU   \n\n");
-		printf("[a] Criar Contato \n");
-		printf("[b] Pesquisar Contato \n");
-		printf("[c] Excluir Contato \n");
-		printf("[d] Fechar Agenda\n\n");
-		printf("Digite a sua opcao: ");
-		scanf("%c",&opcao);
-		fflush(stdin);
+        printf("       AGENDA DE CONTATOS - MENU   \n\n");
+        printf("[a] Criar Contato \n");
+        printf("[b] Pesquisar Contato \n");
+        printf("[c] Excluir Contato \n");
+        printf("[d] Fechar Agenda\n\n");
+        printf("Digite a sua opcao: ");
+        scanf("%c",&opcao);
+        fflush(stdin);
 
-		switch(opcao) {
-			case 'a':
+        switch(opcao) {
+            case 'a':
                 addContato();
-				break;
-			case 'b':
-				break;
-			case 'c':
-				break;
-			case 'd':
-				break;
+                break;
+            case 'b':
+                pesquisarContato();
+                break;
+            case 'c':
+                excluirContato();
+                break;
+            case 'd':
+                break;
             default:
                 printf("Opcao invalida! Tente novamente. \n");
                 break;
-		}
+        }
     } while (opcao != 'd');
 
     printf("\nSaindo...\n");
 
     return 0;
+}
+
+
+//Funções
+void addContato(void){
+    
+    Contato AddContato; //Define estrutura "Nome" e "Numero"
+    File *contatos; //Declarar variavel "contatos"
+
+        contatos = fopen("contatos.txt","????")
+        if (contatos == NULL) {
+    
+        printf("Erro ao abrir arquivo.\n");
+        exit(1)
+
+}
+
+    printf("\nDigite o nome do contato: ");
+    scanf(" %c[^\n]", AddContato.nome);
+    fprintf(contatos, "", AddContato.nome);
+    printf("\nDigite o numero do telefone: ");
+    scanf(" %i[^\n]", AddContato.numero);
+     fprintf(contatos, "", AddContato.numero);
+    fclose(contatos);
+    printf("Contato adicionado\n");
 }
